a7fb5d74bf21e345e643119b314570355fd8a8d4
ShpilevskyAndrei <103843433+ShpilevskyAndrei@users.noreply.github.com>
2022-08-08 11:09:16 +0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ express = require("express");
 path = require("path");
 app = express();
 app.use(express.static("./dist/cv"));
-app.get("/*", (req, res) =>
-    res.sendFile("index.html", { root: "dist/cv/" })
-);
+app.get("/*", function (req, res) {
+    res.sendFile("index.html", { root: "dist/cv/" });
+});
 app.listen(process.env.PORT || 8080);
