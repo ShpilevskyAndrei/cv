56861af632b01ffa444d2914b1ef3279702b173d
ShpilevskyAndrei <103843433+ShpilevskyAndrei@users.noreply.github.com>
2022-08-08 10:04:18 +0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static("./dist/angular-app-heroku"));
-app.get("/*", (req, res) =>
-    res.sendFile("index.html", { root: "dist/angular-app-heroku/" })
-);
+app.use(express.static("./dist/cv"));
+app.get("/*", (req, res) => res.sendFile("index.html", { root: "dist/cv/" }));
 app.listen(process.env.PORT || 8080);
