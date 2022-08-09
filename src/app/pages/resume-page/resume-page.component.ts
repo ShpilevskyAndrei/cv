2a1fbf4d07cb87dfb563b81d70c6606de466ee5d
ShpilevskyAndrei <103843433+ShpilevskyAndrei@users.noreply.github.com>
2022-08-09 09:40:03 +0300

    updates

--- src/app/pages/resume-page/resume-page.component.ts
+++ src/app/pages/resume-page/resume-page.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 export class ResumePageComponent {
     public birthday = new Date('1991.11.28');
     public timeDiff = Math.abs(Date.now() - this.birthday.getTime());
-    public age = Math.floor(this.timeDiff / (1000 * 3600 * 24) / 365.25);
+    public age = Math.floor(this.timeDiff / (1000 * 3600 * 24) / 365.25) || 30;
 }
